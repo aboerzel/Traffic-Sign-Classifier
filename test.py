e6acfb46558e41149a0918df3ea89d50debb4eff
Andreas Börzel <andreas.boerzel@gmx.de>
2018-12-27 22:56:21 +0100

    first lenet model

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 import pickle
+import numpy as np
+
+import matplotlib.pyplot as plt
+import random
 
 import keras
 import tensorflow as tf
@@ -22,3 +26,21 @@ X_valid, y_valid = valid['features'], valid['labels']
 X_test, y_test = test['features'], test['labels']
 
 print("Train: " + str(len(X_train)))
+
+labels = np.unique(y_train)
+train_grouped = np.array([list(X_train[y_train[0:] == i]) for i in labels])
+
+# show image of 10 random data points
+fig, axs = plt.subplots(len(labels), 5, figsize=(10, 200))
+#fig.subplots_adjust(hspace=.2, wspace=.001)
+axs = axs.ravel()
+for l in labels:
+    group = train_grouped[l]
+    for i in range(5):
+        image = group[random.randint(0, len(group)-1)]
+        index = l * 5 + i
+        axs[index].axis('off')
+        axs[index].imshow(image)
+        axs[index].set_title(y_train[index])
+
+plt.show()
