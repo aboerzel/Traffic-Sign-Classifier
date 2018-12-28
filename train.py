c836d173cf32716381706e80977a2dcbe409ad81
Andreas Börzel <andreas.boerzel@gmx.de>
2018-12-28 10:02:05 +0100

    Added train.py to test the architecture of the Lenet model

---
+++ train.py
@@ -0,0 +1,75 @@
+# import the necessary packages
+import pickle
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Flatten
+from keras.layers.core import Dense
+from keras import backend as K
+
+
+class LeNet:
+    @staticmethod
+    def build(classes):
+        model = Sequential()
+
+        # Layer 1
+        # Conv Layer 1 => 28x28x6
+        model.add(Conv2D(filters=6, kernel_size=5, strides=1, activation='relu', input_shape=(32, 32, 1)))
+
+        # Layer 2
+        # Pooling Layer 1 => 14x14x6
+        model.add(MaxPooling2D(pool_size=2, strides=2))
+
+        # Layer 3
+        # Conv Layer 2 => 10x10x16
+        model.add(Conv2D(filters=16, kernel_size=5, strides=1, activation='relu', input_shape=(14, 14, 6)))
+
+        # Layer 4
+        # Pooling Layer 2 => 5x5x16
+        model.add(MaxPooling2D(pool_size=2, strides=2))
+
+        # Flatten
+        model.add(Flatten())
+
+        # Layer 5
+        # Fully connected layer 1 => 120x1
+        model.add(Dense(units=120, activation='relu'))
+
+        # Layer 6
+        # Fully connected layer 2 => 84x1
+        model.add(Dense(units=84, activation='relu'))
+
+        # Output Layer => classes x 1
+        model.add(Dense(units=classes, activation='softmax'))
+
+        model.summary()
+        return model
+
+
+training_file = '../data/train.p'
+validation_file = '../data/valid.p'
+testing_file = '../data/test.p'
+
+with open(training_file, mode='rb') as f:
+    train = pickle.load(f)
+with open(validation_file, mode='rb') as f:
+    valid = pickle.load(f)
+with open(testing_file, mode='rb') as f:
+    test = pickle.load(f)
+
+X_train, y_train = train['features'], train['labels']
+X_valid, y_valid = valid['features'], valid['labels']
+X_test, y_test = test['features'], test['labels']
+
+classes = len(np.unique(y_train))
+
+model = LeNet.build(classes)
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(X_train, y_train, validation_data=(X_valid, y_valid), batch_size=64, epochs=42)
+
+y_pred = model.predict(X_test)
