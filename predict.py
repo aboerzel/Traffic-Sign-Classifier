4da97bdc23a043e6b681e985df224eda17d19e39
Andreas Börzel <andreas.boerzel@gmx.de>
2018-12-29 13:43:33 +0100

    prediction changed

--- predict.py
+++ predict.py
@@ -3,6 +3,7 @@ import pickle
 import keras
 from keras.models import load_model
 
+# load test dataset
 testing_file = '../data/test.p'
 
 with open(testing_file, mode='rb') as f:
@@ -10,17 +11,19 @@ with open(testing_file, mode='rb') as f:
 
 X_test, y_test = test['features'], test['labels']
 
+# prepare data for network
 X_test = X_test.reshape((X_test.shape[0], 32, 32, 3))
 X_test = X_test.astype('float32') / 255
 
 num_classes = 43
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
+# load traind model
 model = load_model('./output/traffic_sings_model.h5')
 
 # predict
 y_pred = model.predict(X_test)
 
-# show the true and predicted class for the test dataset
+# show the true and the predicted classes for the test dataset
 for i in range(len(y_test)):
-    print("{:4d} : True={}, Predicted={}".format(i, y_test[i], y_pred[i]))
+    print("{:4d} : True={: <2}, Predicted={}".format(i, y_test[i].argmax(axis=-1), y_pred[i].argmax(axis=-1)))
