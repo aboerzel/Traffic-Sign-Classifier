8e2c1cbb89c0232e4afd52208bb63dee8dd0e1c6
Andreas Börzel <andreas.boerzel@gmx.de>
2018-12-29 11:11:27 +0100

    load sign names from csv

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 # import the necessary packages
+import csv
 import pickle
 
 import Augmentor
@@ -12,10 +13,16 @@ from keras.layers.core import Flatten
 from keras.models import Sequential
 from keras.optimizers import SGD, Adam
 
+batch_size = 64
+
+# optimizer = SGD(lr=0.01)
+# optimizer = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+optimizer = Adam(lr=1e-4, clipnorm=0.001)
+
 
 class LeNet:
     @staticmethod
-    def build(classes):
+    def build(num_classes):
         model = Sequential()
 
         # Layer 1
@@ -28,7 +35,7 @@ class LeNet:
 
         # Layer 3
         # Conv Layer 2 => 10x10x16
-        model.add(Conv2D(filters=16, kernel_size=5, strides=1, activation='relu'))
+        model.add(Conv2D(filters=16, kernel_size=5, strides=1, activation='relu', input_shape=(14, 14, 6)))
 
         # Layer 4
         # Pooling Layer 2 => 5x5x16
@@ -49,8 +56,8 @@ class LeNet:
 
         model.add(Dropout(0.5))
 
-        # Output Layer => classes x 1
-        model.add(Dense(units=classes, activation='softmax'))
+        # Output Layer => num_classes x 1
+        model.add(Dense(units=num_classes, activation='softmax'))
 
         model.summary()
         return model
@@ -71,43 +78,54 @@ X_train, y_train = train['features'], train['labels']
 X_valid, y_valid = valid['features'], valid['labels']
 X_test, y_test = test['features'], test['labels']
 
+
+# load classes and sign names from csv file
+def load_signnames_from_csv(filename):
+    rows = []
+    with open(filename) as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        next(reader)  # skip header
+        for row in reader:
+            class_id = row[0]
+            sign_name = row[1]
+            rows.append((class_id, sign_name))
+
+    return np.array(rows)
+
+
+sign_names = load_signnames_from_csv('signnames.csv')
+
+# prepare data for network
 X_train = X_train.reshape((X_train.shape[0], 32, 32, 3))
-x_train = X_train.astype('float32')
-x_train /= 255
+x_train = X_train.astype('float32') / 255
 
-classes = len(np.unique(y_train))
-y_train = keras.utils.to_categorical(y_train, classes)
+X_valid = X_valid.reshape((X_valid.shape[0], 32, 32, 3))
+X_valid = X_valid.astype('float32') / 255
 
+X_test = X_test.reshape((X_test.shape[0], 32, 32, 3))
+X_test = X_test.astype('float32') / 255
+
+num_classes = len(sign_names)
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_valid = keras.utils.to_categorical(y_valid, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+# image augmentation
 p = Augmentor.Pipeline()
 p.skew(probability=0.5, magnitude=0.1)
 p.zoom(probability=0.5, min_factor=0.8, max_factor=1.2)
 p.rotate(probability=0.8, max_left_rotation=5, max_right_rotation=5)
 
-datagen1 = p.keras_generator_from_array(X_train, y_train, batch_size=32)
+datagen = p.keras_generator_from_array(X_train, y_train, batch_size=batch_size)
 
-# datagen = ImageDataGenerator(
-# #     featurewise_center=True,
-# #     featurewise_std_normalization=True,
-# #     rotation_range=20,
-# #     width_shift_range=0.2,
-# #     height_shift_range=0.2,
-# #     zoom_range=0.2,
-# #     data_format='channels_last')
-
-#datagen1.fit(X_train)
-
-model = LeNet.build(classes)
-opt = SGD(lr=0.01)
-sgd = SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)
-optimizer = Adam(lr=1e-4, clipnorm=0.001)
-model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+# build LeNet model
+model = LeNet.build(num_classes)
 
 # The function to optimize is the cross entropy between the true label and the output (softmax) of the model
-# We will use adadelta to do the gradient descent see http://cs231n.github.io/neural-networks-3/#ada
-#model.compile(loss='categorical_crossentropy', optimizer='adamax', metrics=["accuracy"])
+model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
 
-# model.compile(loss=keras.losses.categorical_crossentropy, optimizer=’SGD’, metrics=[“accuracy”])
+# train model
+model.fit_generator(datagen, validation_data=(X_valid, y_valid), steps_per_epoch=len(X_train) / batch_size, epochs=100)
 
-model.fit_generator(datagen1, steps_per_epoch=len(X_train) / 32, epochs=100)
-
+# predict
 y_pred = model.predict(X_test)
