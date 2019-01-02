6cd088dd6a633fadda234a2b1d6a7140a02dcd93
Andreas Börzel <andreas.boerzel@gmx.de>
2019-01-02 10:53:51 +0100

    print top 5 predictions

--- predict.py
+++ predict.py
@@ -61,10 +61,29 @@ fig, axs = plt.subplots(rows, cols, figsize=(10, 200))
 axs = axs.ravel()
 
 for i, (filename, image, org_image) in enumerate(zip(filenames, X_test, original_images)):
-    class_name = sign_names[y_pred.argmax(axis=-1)[i]][1]
+    class_id = y_pred.argmax(axis=-1)[i]
+    class_name = sign_names[class_id][1]
     print("{} => {}".format(filename, class_name))
     axs[i].axis('off')
     axs[i].imshow(org_image)
-    axs[i].set_title(class_name)
+    axs[i].set_title('{}: {}'.format(class_id, class_name))
 
+plt.show()
+
+# print top 5 predictions
+k = 5
+n = len(filenames)
+plt.figure(figsize=(15, 16))
+for i, (filename, prob, org_image) in enumerate(zip(filenames, y_pred, original_images)):
+    top_values_index = sorted(range(len(prob)), key=lambda p: prob[p])[-k:]
+    class_id = prob.argmax(axis=-1)
+    class_name = sign_names[class_id][1]
+    plt.subplot(n, 2, 2 * i + 1)
+    plt.imshow(original_images[i])
+    plt.title(filename)
+    plt.axis('off')
+    plt.subplot(n, 2, 2 * i + 2)
+    plt.barh(np.arange(1, 6, 1), prob[top_values_index])
+    labels = np.array([sign_names[j] for j in top_values_index])
+    plt.yticks(np.arange(1, 6, 1), labels[:, 1])
 plt.show()
